31b013aec114f379d18cfdbc42bd486e190fc145
XuGang <garyxu@qq.com>
2024-06-22 18:29:50 +0800

    .gitignore

--- tests/load_config_test.go
+++ tests/load_config_test.go
@@ -3,9 +3,15 @@ package tests
 import (
 	"testing"
 	"wechat_llm/config"
+	config2 "wechat_llm/quant/config"
 )
 
 func TestLoadConfig(t *testing.T) {
 	apiKey := config.LoadConfig().ApiKey
+	t.Log("apiKey is: ", apiKey)
+}
+
+func TestLoadConfig2(t *testing.T) {
+	apiKey := config2.LoadOKXConfig().ApiKey
 	t.Log("apiKey is: ", apiKey)
 }
